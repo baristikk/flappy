33504e169a9abfca8cf4a254e248d46450744b73
baristikk <filipea133@gmail.com>
2017-02-21 11:53:25 -0300

    Comecando a programacao do movimento da prancha

---
+++ Assets/Scripts/PranchaMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PranchaMove : MonoBehaviour {
+
+    public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocidadeVetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+		
+	}
+}
